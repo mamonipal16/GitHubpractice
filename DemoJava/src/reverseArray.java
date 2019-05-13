d88efd942cfd31fe5c7d969359cfb70541be2be1
Mamoni <mamonipal93@gmail.com>
2019-05-14 00:15:39 +0530

    2

---
+++ DemoJava/src/reverseArray.java
@@ -0,0 +1,11 @@
+
+public class reverseArray {
+
+public static void main(String args[]) {
+	int a[]= {10,20,30,40};
+	 int n=a.length;
+for (int i=n-1;i>=0; i--) {
+	System.out.println(a[i]+"  ");
+}
+}
+}
